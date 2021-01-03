df50430a0e1dba86a089341ed44f5f73c6334822
furtado3g <lucas_shiguioka@hotmail.com>
2021-01-03 15:50:58 -0300

    created ref to heroku's database

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,13 @@ module.exports = {
   client: "pg",
   version: "7.2",
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'Therev a7x',
-    database: 'poetry',
-    /* ssl: {
+    host: process.env.DATABASE_HOST || "127.0.0.1",
+    user: process.env.DATABASE_USER || "postgres",
+    password: process.env.DATABASE_PSW || "Therev a7x",
+    database: process.env.DATABASE_NAME || "poetry",
+    ssl: {
       rejectUnauthorized: false,
-    }, */
+    },
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "databases", "migrations"),
